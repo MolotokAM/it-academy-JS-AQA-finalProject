26738aeaa9849aabd325465ed35e8dc8faf0761c
Artur <2329494@gmail.com>
2022-08-23 18:00:53 +0300

    added final homework - test framework for automated Amazon testing

---
+++ src/test_scripts/loginSuccessfully.spec.js
@@ -0,0 +1,14 @@
+import AuthFlow from '../test_flow/AuthFlow';
+import User from '../entities/User';
+
+const userTest = new User('khiemltv@gmail.com', 'khiemluc2903')
+
+describe(`Testing 'Login' application`, () => {
+    it('should login with valid credentials', () => {
+        const Auth = new AuthFlow(userTest.email, userTest.password)
+        Auth
+            .openLoginPage()
+            .doLogin()
+            .verifyNavigateToNotificationPage()
+    });
+});
---
+++ src/test_scripts/loginUnsuccessfully.spec.js
@@ -0,0 +1,14 @@
+import AuthFlow from '../test_flow/AuthFlow';
+import User from '../entities/User';
+
+const userTest = new User('khiemltv@gmail.com', 'invalid')
+
+describe(`Testing 'Login' application`, () => {
+    it('should cannot login with invalid credentials', () => {
+        const Auth = new AuthFlow(userTest.email, userTest.password)
+        Auth
+            .openLoginPage()
+            .doLogin()
+            .verifyAlertWhenInputInvalidPassword()
+    });
+});
